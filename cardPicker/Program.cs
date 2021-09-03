80b79010c9815f3d49895f3c59dab0966e45ba00
praveen <praveenthakur388@gmail.com>
2021-09-03 18:09:48 +0530

    Add project files.

---
+++ cardPicker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cardPicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick:");
+            string line = Console.ReadLine();
+            int numberOfCards = Convert.ToInt32(line);
+            if (numberOfCards > 52)
+            {
+                Console.WriteLine("there is only 52 cards in a deck");
+            }
+            else
+            {
+                foreach(string card in randomCardPicker.pickCard(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+
+        }
+    }
+}
---
+++ cardPicker/randomCardPicker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardPicker
+{
+    class randomCardPicker
+    {
+        static Random random = new Random();
+        public static string[] pickCard(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for(int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = randomValue() + " of " + randomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string randomValue()
+        {
+            int value = random.Next(1,14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "jack";
+            if (value == 12) return "Queen";  
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+
+        private static string randomSuit()
+        {
+            int value = random.Next(1,5);
+            if (value == 1) return "Clubs";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Spades";
+            return "Dimond";
+        }
+    }
+}
